735e143d8d1a62ec499ed5e80c5d96b7fd8b475d
Eric Fries <ericfries@Erics-MacBook-Pro-2.local>
2018-10-07 17:24:49 -0400

    fix quantize_decimal util

--- states/utils.py
+++ states/utils.py
@@ -1,5 +1,5 @@
-from decimal import ROUND_DOWN
+from decimal import Decimal, ROUND_DOWN
 
 
 def quantize_decimal(raw_decimal, precision='.01', rounding_option=ROUND_DOWN):
-    return raw_decimal.quantize('.01', rounding_option)
+    return raw_decimal.quantize(Decimal(precision), rounding_option)
